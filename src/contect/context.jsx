d7decbcb34d401399116c6f30baa011219ba66c2
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2024-12-09 20:33:31 +0200

    implement search dog by selected breed id

--- src/contect/context.jsx
+++ src/contect/context.jsx
@@ -4,45 +4,64 @@ const DogDataContext = createContext();
 
 export const DogDataProvider = ({ children }) => {
   const [dogData, setDogData] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
+  const [dogBreeds, setDogBreeds] = useState([]);
+  const [selectedBreedId, setSelectedBreedId] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchDogs = async (url) => {
-      const apiKey = import.meta.env.VITE_DOG_API_KEY;
-      if (!apiKey) {
-        throw new Error("API key is missing");
-      }
-      try {
-        setLoading(true);
-        setError(null);
-        const response = await fetch(url, {
-          method: "GET",
-          headers: {
-            "x-api-key": apiKey,
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        setDogData(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchDogs("https://api.thecatapi.com/v1/images/search?limit=10");
+    fetchDogs(
+      "https://api.thecatapi.com/v1/images/search?limit=10",
+      setDogData
+    );
+    fetchDogs("https://api.thecatapi.com/v1/breeds", setDogBreeds);
   }, []);
+
+  useEffect(() => {
+    fetchDogs(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${selectedBreedId}`,
+      setDogData
+    );
+  }, [selectedBreedId]);
+
+  const fetchDogs = async (url, setState) => {
+    const apiKey = import.meta.env.VITE_DOG_API_KEY;
+    if (!apiKey) {
+      throw new Error("API key is missing");
+    }
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "x-api-key": apiKey,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setState(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <DogDataContext.Provider
-      value={{ dogData, searchInput, setSearchInput, loading, error }}
+      value={{
+        dogData,
+        setSelectedBreedId,
+        dogBreeds,
+        loading,
+        error,
+      }}
     >
       {children}
     </DogDataContext.Provider>
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,31 +1,41 @@
 import React, { useContext } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import Result from "../../components/results/Results";
 import DogDataContext from "../../contect/context";
 
 const Search = () => {
-  const { searchInput, setSearchInput } = useContext(DogDataContext);
-
-  const handleChange = (e) => {
-    setSearchInput(e.target.value);
-  };
+  const { dogBreeds, setSelectedBreedId } = useContext(DogDataContext);
 
   return (
-    <section>
-      <Form
-        onSubmit={(e) => e.preventDefault()}
-        className="d-flex justify-content-center my-4"
-      >
-        <Form.Control
-          type="text"
-          placeholder="Search for a dog"
-          value={searchInput}
-          onChange={handleChange}
-          className="mr-2"
-        />
-      </Form>
+    <section className="my-4">
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <Form
+            onSubmit={(e) => e.preventDefault()}
+            className="d-flex flex-column align-items-center"
+          >
+            <Form.Group controlId="dogBreedSelect">
+              <Form.Label>Search for a dog by breed</Form.Label>
+              <Form.Select
+                onChange={(e) => {
+                  setSelectedBreedId(e.target.value);
+                }}
+                aria-label="Search for a dog by breed"
+              >
+                <option value="">Select a breed</option>
+                {dogBreeds.map((breed) => (
+                  <option key={breed.id} value={breed.id}>
+                    {breed.name}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
       <Result />
     </section>
   );
 };
+
 export default Search;
